1ed9abad229eb487525dc29ef2b1a8914226d509
sachika <s.tsukeyanagi.sys23@morijyobi.ac.jp>
2024-07-30 18:20:53 +0900

    CPUの手をランダムにする処理を実装 close #15,close #16

--- src/Player.java
+++ src/Player.java
@@ -1,23 +1,98 @@
+import java.util.Random;
+
+
+
 public class Player {
 
-    private String playerName;
+   private String playerName;
 
-    public Player(String playerName) {
+   private Hand hand;
 
-        this.playerName = playerName;
 
-    }
 
-    public String getPlayerName() {
+   public Player(String playerName) {
 
-        return playerName;
+       this.playerName = playerName;
 
-    }
+   }
 
-    public void setPlayerName(String playerName) {
 
-        this.playerName = playerName;
 
-    }
+   /**
+
+    手の強弱を比較する。
+
+    返り値 -1 : 自身が引数で指定した手より弱い
+
+    　　　　0 : 自身が引数で指定した手と同じ
+
+    　　　　1 : 自身が引数で指定した手より強い
+
+    */
+
+   public void createRandomHand() {
+
+       
+
+       Random random = new Random();
+
+       int randomValue = random.nextInt(3);
+
+
+
+       if(randomValue == 0) {
+
+           this.hand = new Gu();
+
+       } else if(randomValue == 1) {
+
+           this.hand = new Choki();
+
+       } else {
+
+           this.hand = new Pa();
+
+       }
+
+   }
+
+
+
+   public String getPlayerName() {
+
+       return this.playerName;
+
+   }
+
+
+
+   public void setPlayerName(String name) {
+
+       this.playerName = name;
+
+   }
+
+
+
+   public Hand getHand() {
+
+       return this.hand;
+
+   }
+
+
+
+   public void setHand(Hand hand) {
+
+       this.hand = hand;
+
+   }
 
 }
+
+//以降にテストコードを追加して検証を行う
+
+System.out.println("ランダムなジャンケンの手をCPUにセットする");
+
+System.out.println(com.getHand().getHandName());
+
